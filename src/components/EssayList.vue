884593eb557a6afbf6f9e531b9ea464ab20284b7
Guilherme Passero <guilherme.passero0@gmail.com>
2017-06-18 16:38:04 -0300

    Essays's scores listed with colors

--- src/components/EssayList.vue
+++ src/components/EssayList.vue
@@ -22,6 +22,12 @@
                 <td>{{ getPrompt(props.item).date | moment('YYYY/MM') }}</td>
                 <td>{{ props.item.title }}</td>
                 <td>{{ props.item.text | makeBrief }}</td>
+                <td v-for="i in [1, 2, 3, 4, 5, 0]">
+                    <v-chip class="white--text" :class="getScoreColor(props.item, i)">
+                        {{ getScore(props.item, i).toFixed(1) }}
+                        <v-icon v-if="i==0" right>{{ getFinalScoreIcon(props.item) }}</v-icon>
+                    </v-chip>
+                </td>
             </template>
         </v-data-table>
     </v-card>
@@ -34,14 +40,27 @@ export default {
             headers: [
                 { text: 'Data', left: true, value: 'date' },
                 { text: 'Título', left: true, value: 'title' },
-                { text: 'Texto', left: true, value: 'text' }
+                { text: 'Texto', left: true, value: 'text' },
+                { text: 'Nota 1', left: true, value: 'score1' },
+                { text: 'Nota 2', left: true, value: 'score2' },
+                { text: 'Nota 3', left: true, value: 'score3' },
+                { text: 'Nota 4', left: true, value: 'score4' },
+                { text: 'Nota 5', left: true, value: 'score5' },
+                { text: 'Nota final', left: true, value: 'score' }
             ],
             search: '',
             pagination: {
                 rowsPerPage: 10,
                 descending: true
             },
-            loading: true
+            loading: true,
+            scoreColors: {
+                '2.0': 'green',
+                '1.5': 'blue',
+                '1.0': 'yellow',
+                '0.5': 'orange',
+                '0.0': 'red'
+            }
         }
     },
     computed: {
@@ -64,6 +83,28 @@ export default {
                 return '';
             }
             return this.prompts[essay.prompt.uid];
+        },
+        getScore(essay, number) {
+            if (number != 0) {
+                return essay.scores['Competência '+number];
+            }
+            return essay.total_score;
+        },
+        getScoreColor(essay, number) {
+            let score = this.getScore(essay, number);
+            if (number == 0) {
+                score = Math.round(2 * score / 5) / 2;
+            }
+            return this.scoreColors[score.toFixed(1)];
+        },
+        getFinalScoreIcon(essay) {
+            let score = this.getScore(essay, 0).toFixed(1);
+            if (score == 10) {
+                return 'star';
+            }
+            if (score == 0) {
+                return 'block';
+            }
         }
     }
 }
